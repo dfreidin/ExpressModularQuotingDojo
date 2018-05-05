5e000442f60788e5b7a286da12a7755d49d31d5d
dfreidin <36039896+dfreidin@users.noreply.github.com>
2018-05-05 03:19:11 -0700

    modularized models

--- server.js
+++ server.js
@@ -7,10 +7,6 @@ app.set("views", path.join(__dirname, "./views"));
 app.set("view engine", "ejs");
 app.use(body_parser.urlencoded({extended: true}));
 mongoose.connect("mongodb://localhost/quoting_dojo");
-var QuoteSchema = new mongoose.Schema({
-    name: {type: String, required: true, minlength: 2},
-    quote: {type: String, required: true, minlength: 2},
-}, {timestamps: true});
-mongoose.model("Quote", QuoteSchema);
+require("./server/models/quote")(mongoose);
 require("./server/config/routes")(app);
 app.listen(8000);
---
+++ server/models/quote.js
@@ -0,0 +1,7 @@
+module.exports = function(mongoose) {
+    var QuoteSchema = new mongoose.Schema({
+        name: {type: String, required: true, minlength: 2},
+        quote: {type: String, required: true, minlength: 2},
+    }, {timestamps: true});
+    mongoose.model("Quote", QuoteSchema);
+}
